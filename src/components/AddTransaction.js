6183bda17c304b497089613fa2cba6665f3ff30b
noamansaleem <noamantech@gmail.com>
2021-01-23 21:59:07 +0500

    clearing input values after add transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -17,6 +17,8 @@ export const AddTransaction = () => {
         }
         // console.log(transactionData)
         addTransaction(transactionData);
+        setDescription('');
+        setTransactionAmount('');
     }
 
 
